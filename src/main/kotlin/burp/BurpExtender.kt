1ef5b0239a7d52aac8caa08a0441e73b85157444
Philippe Arteau <parteau@gosecure.ca>
2019-02-26 15:46:15 -0500

    Redirect minlog to the extension dedicated log and show alerts when something critical happens.

--- src/main/kotlin/burp/BurpExtender.kt
+++ src/main/kotlin/burp/BurpExtender.kt
@@ -1,5 +1,8 @@
 package burp
 
+import com.esotericsoftware.minlog.Log
+import java.io.IOException
+
 class BurpExtender : IBurpExtender, IMessageEditorTabFactory {
 
     lateinit var callbacks: IBurpExtenderCallbacks
@@ -11,6 +14,23 @@ class BurpExtender : IBurpExtender, IMessageEditorTabFactory {
         helpers = callbacks.helpers
 
         callbacks.setExtensionName("NTLM Challenge Decoder")
+
+
+        Log.setLogger(object : Log.Logger() {
+            override fun print(message: String) {
+                try {
+                    if (message.contains("ERROR:")) { //Not the most elegant way, but should be effective.
+                        callbacks.issueAlert(message)
+                    }
+                    callbacks.stdout.write(message.toByteArray())
+                    callbacks.stdout.write('\n'.toInt())
+                } catch (e: IOException) {
+                    System.err.println("Error while printing the log : " + e.message) //Very unlikely
+                }
+
+            }
+        })
+        Log.INFO()
 
         callbacks.registerMessageEditorTabFactory(this)
     }
